564832300bb61560c1174c4d8c48b18a28c3b36b
Eric <ericobrien246@gmail.com>
2023-05-02 21:53:58 +0100

    Made stars have a trail, ship rotates in circle

--- java/src/C21406436/Rocket.java
+++ java/src/C21406436/Rocket.java
@@ -14,8 +14,42 @@ public class Rocket extends Visual {
     ArrayList<PVector> trailPositions = new ArrayList<>();
     int trailLength = 50;
 
+    float circleRadius = 200;
+    float circleAngle = 0;
+
+    float planetSpeed = 0.2f;
+
+    class Star {
+        PVector position;
+        float speed;
+        int trailLength;
+
+        Star(float x, float y, float speed, int trailLength) {
+            this.position = new PVector(x, y);
+            this.speed = speed;
+            this.trailLength = trailLength;
+        }
+
+        void update() {
+            position.y += speed;
+            if (position.y > height) {
+                position.y = 0;
+                position.x = random(width);
+            }
+        }
+
+        void display() {
+            for (int i = 0; i < trailLength; i++) {
+                fill(0, 0, 255, 255 * (1 - (i / (float) trailLength)));
+                ellipse(position.x, position.y - i * speed, 1, 1);
+            }
+        }
+    }
+
+    ArrayList<Star> stars = new ArrayList<>();
+
     public void settings() {
-        size(800, 600);
+        size(800, 800);
     }
 
     public void setup() {
@@ -36,34 +70,48 @@ public class Rocket extends Visual {
 
     public void draw() {
         background(0);
-
+    
         drawStars(100);
 
-        int planetColor = color(127 * sin(radians(planetColorAngle)) + 128,
+        for (Star star : stars) {
+            star.update();
+            star.display();
+        }
+    
+        float planetSize = 80 + 10 * sin(radians(planetSizeAngle));
+        float planetColor = color(127 * sin(radians(planetColorAngle)) + 128,
                 127 * sin(radians(planetColorAngle + 120)) + 128, 127 * sin(radians(planetColorAngle + 240)) + 128);
-        float planetSizeOffset = 10 * sin(radians(planetSizeAngle));
-        drawPlanet(300, 100, 80 + planetSizeOffset, planetColor);
-
-        // Update floatX and floatY with Perlin noise values
-        float floatX = map(noise(time), 0, 1, -width / 2, width / 2);
-        float floatY = map(noise(time + 10000), 0, 1, -height / 2, height / 2);
-
+    
+        // Modify the map() function to use the planetSpeed variable
+        float planetX = map(sin(radians(frameCount * planetSpeed)), -1, 1, -width, width);
+    
+        drawPlanet(planetX, 100, planetSize, (int) planetColor); // Change argument order and cast planetColor to int
+    
+        // Calculate the position of the spaceship moving in a circle
+        float spaceshipX = circleRadius * cos(radians(circleAngle));
+        float spaceshipY = circleRadius * sin(radians(circleAngle));
+    
         translate(width / 2, height / 2);
-        translate(floatX, floatY);
+        translate(spaceshipX, spaceshipY);
         drawSpaceship();
+    
+        circleAngle += 0.2f;
+        planetColorAngle += 0.1f;
+        planetSizeAngle += 0.5f;
 
-        floatAngle += 0.5;
-        planetColorAngle += 0.5;
-        planetSizeAngle += 1;
-        time += 0.001; // Increment time value for Perlin noise
+       
     }
+    
 
     public void drawStars(int numStars) {
-        fill(255);
-        for (int i = 0; i < numStars; i++) {
-            float x = random(width);
-            float y = random(height);
-            ellipse(x, y, 1, 1);
+        if (stars.size() < numStars) {
+            for (int i = stars.size(); i < numStars; i++) {
+                float x = random(width);
+                float y = random(height);
+                float speed = random(1, 3);
+                int trailLength = (int) random(5, 20);
+                stars.add(new Star(x, y, speed, trailLength));
+            }
         }
     }
 
@@ -71,33 +119,42 @@ public class Rocket extends Visual {
         pushMatrix();
         translate(x, y);
         noStroke();
-
-        // Draw the planet with shading
-        for (int i = 0; i < 360; i += 10) {
-            float angle = radians(i);
-            float xOff = cos(angle) * size / 2;
-            float yOff = sin(angle) * size / 2;
-            int shade = color(hue(planetColor), saturation(planetColor), brightness(planetColor) * (1 - (i / 360.0f)));
-            fill(shade);
-            ellipse(xOff, yOff, size, size);
+        colorMode(HSB);
+        
+        // Draw the planet with shading and gradient
+        for (int i = 0; i < 360; i += 5) {
+          float angle = radians(i);
+          float xOff = cos(angle) * size / 2;
+          float yOff = sin(angle) * size / 2;
+          float distance = dist(xOff, yOff, 0, 0);
+          int shade = color(hue(planetColor), map(distance, 0, size/2, 0, 100), brightness(planetColor) * (1 - (i / 360.0f)));
+          fill(shade);
+          ellipse(xOff, yOff, size, size);
         }
-
+      
         // Draw a textured overlay
         for (int i = 0; i < 50; i++) {
-            float xPos = random(-size / 2, size / 2);
-            float yPos = random(-size / 2, size / 2);
-            float craterSize = random(3, 8);
-            float brightnessOffset = random(-20, 20);
-            int textureColor = color(hue(planetColor), saturation(planetColor),
-                    brightness(planetColor) + brightnessOffset);
-            fill(textureColor);
-            ellipse(xPos, yPos, craterSize, craterSize);
+          float xPos = random(-size / 2, size / 2);
+          float yPos = random(-size / 2, size / 2);
+          float craterSize = random(3, 8);
+          float brightnessOffset = random(-20, 20);
+          int textureColor = color(hue(planetColor), saturation(planetColor), brightness(planetColor) + brightnessOffset, 200);
+          fill(textureColor);
+          ellipse(xPos, yPos, craterSize, craterSize);
         }
-
+      
+        // Draw a glowing aura around the planet
+        for (int i = 0; i < 10; i++) {
+          int glowColor = color(hue(planetColor), 100, 100, 50 - i*5);
+          fill(glowColor);
+          ellipse(0, 0, size + i*10, size + i*10);
+        }
+        
         popMatrix();
-    }
+      }
+      
 
-    public void drawSpaceship() {
+      public void drawSpaceship() {
         fill(0, 0, 255); // Set the spaceship color using HSB values
         
         // Body of the spaceship
@@ -106,28 +163,29 @@ public class Rocket extends Visual {
         vertex(-10, 10);
         vertex(10, 10);
         endShape(CLOSE);
-
+    
         // Left wing
         beginShape();
         vertex(-10, 10);
         vertex(-30, 20);
         vertex(-20, 10);
         endShape(CLOSE);
-
+    
         // Right wing
         beginShape();
         vertex(10, 10);
         vertex(30, 20);
         vertex(20, 10);
         endShape(CLOSE);
-
-       // Engine trail
-       float engineTrailSize = 10 + 5 * sin(radians(millis() / 5));
-       fill(255, 225, 180);
-       for (int i = 0; i < 10; i++) {
-           float engineTrailOffset = 10 + 5 * i;
-           ellipse(0, 10 + engineTrailOffset, engineTrailSize, engineTrailSize);
-       }
+    
+        // Engine trail
+        for (int i = 0; i < 10; i++) {
+            float engineTrailSize = 10 + 5 * sin(radians(millis() / 5)) * (1 - (i / 10.0f));
+            float engineTrailOffset = 10 + 5 * i;
+            fill(255, 225, 180, 255 - i * 20); // Added transparency to the trail
+            ellipse(0, 10 + engineTrailOffset, engineTrailSize, engineTrailSize);
+        }
     }
+    
 
 }
