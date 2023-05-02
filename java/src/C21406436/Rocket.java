b96e8e245b4f33e8d72fe1b24cac4e17243e3ea9
Eric <ericobrien246@gmail.com>
2023-05-02 19:38:17 +0100

    Added an engine to spaceship

--- java/src/C21406436/Rocket.java
+++ java/src/C21406436/Rocket.java
@@ -1,19 +1,24 @@
 package C21406436;
 
 import ie.tudublin.Visual;
+import processing.core.PVector;
+
+import java.util.ArrayList;
 
 public class Rocket extends Visual {
 
     float floatAngle = 0;
     float planetColorAngle = 0;
     float planetSizeAngle = 0;
+    ArrayList<PVector> trailPositions = new ArrayList<>();
+    int trailLength = 50;
 
     public void settings() {
         size(800, 600);
     }
 
     public void setup() {
-        colorMode(HSB);
+        //colorMode(HSB);
         noStroke();
         startMinim();
 
@@ -30,19 +35,21 @@ public class Rocket extends Visual {
 
     public void draw() {
         background(0);
-        
+
         drawStars(100);
-        
-        int planetColor = color(127 * sin(radians(planetColorAngle)) + 128, 127 * sin(radians(planetColorAngle + 120)) + 128, 127 * sin(radians(planetColorAngle + 240)) + 128);
+
+        int planetColor = color(127 * sin(radians(planetColorAngle)) + 128,
+                127 * sin(radians(planetColorAngle + 120)) + 128, 127 * sin(radians(planetColorAngle + 240)) + 128);
         float planetSizeOffset = 10 * sin(radians(planetSizeAngle));
         drawPlanet(200, 100, 80 + planetSizeOffset, planetColor);
-        
-        translate(width / 2, height / 2);
+
         float floatX = sin(radians(floatAngle)) * 10;
         float floatY = cos(radians(floatAngle)) * 10;
+
+        translate(width / 2, height / 2);
         translate(floatX, floatY);
-        
         drawSpaceship();
+
         floatAngle += 0.5;
         planetColorAngle += 0.5;
         planetSizeAngle += 1;
@@ -64,7 +71,7 @@ public class Rocket extends Visual {
 
     public void drawSpaceship() {
         fill(255);
-        
+
         // Body of the spaceship
         beginShape();
         vertex(0, -20);
@@ -85,10 +92,13 @@ public class Rocket extends Visual {
         vertex(30, 20);
         vertex(20, 10);
         endShape(CLOSE);
+
+        // Engine flame
+        fill(255, 140, 0);
+        beginShape();
+        vertex(-5, 10);
+        vertex(5, 10);
+        vertex(0, 25);
+        endShape(CLOSE);
     }
 }
-
-
-
-
-
