7c542ca1c223f12946e6d19e06a97fe73efa920a
Ivan <123373115+Gr8ivan@users.noreply.github.com>
2023-05-02 20:18:42 +0100

    Planet & Stars Update

--- java/src/C21406436/Planet.java
+++ java/src/C21406436/Planet.java
@@ -3,6 +3,8 @@ package C21406436;
 import ie.tudublin.Visual;
 import ie.tudublin.VisualException;
 import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PShape;
 
 public class Planet extends Visual {
     float planetSize = 100;
@@ -17,6 +19,10 @@ public class Planet extends Visual {
     float[] starDistances;
     float[][] starPositions;
     float angle = 0;
+    int numShockwaves = 10;
+    float shockwaveThickness = 2;
+    PShape starsSphere;
+    PImage starsTexture;
 
     public void settings() {
         size(1024, 500, P3D);
@@ -34,15 +40,27 @@ public class Planet extends Visual {
         starDistances = new float[numStars];
         starPositions = new float[numStars][3];
 
+        // Draw the stars
         for (int i = 0; i < numStars; i++) {
-            starSizes[i] = random(5, 20);
+            float x = random(width);
+            float y = random(height);
+            float size = random(5, 20);
+            float alpha = map(size, 5, 15, 50, 200);
+            starSizes[i] = size;
             starSpeeds[i] = random(0.0005f, 0.0009f);
             starAngles[i] = random(0, TWO_PI);
             starDistances[i] = random(starDistance * 1f, starDistance * 2.5f);
             starPositions[i][0] = starDistances[i] * cos(starAngles[i]);
             starPositions[i][1] = starDistances[i] * sin(starAngles[i]);
             starPositions[i][2] = random(-5, 5);
+            stroke(255, alpha);
+            noFill();
+            ellipse(x, y, size, size);
         }
+
+        // Create star texture
+        starsTexture = createStarsTexture(2048, 2048, 1000);
+        starsSphere = createStarsSphere(1600, 30, starsTexture);
     }
 
     public void keyPressed() {
@@ -51,6 +69,7 @@ public class Planet extends Visual {
             getAudioPlayer().play();
         }
     }
+
     public void drawPlanet() {
         float bass = getSmoothedBands()[0] * maxPulseSize;
         smoothedBass = smoothingFactor * bass + (1 - smoothingFactor) * smoothedBass;
@@ -75,11 +94,11 @@ public class Planet extends Visual {
             float targetX = distance * cos(angle);
             float targetY = distance * sin(angle);
             float targetZ = random(-5, 5);
-            
+    
             starPositions[i][0] = lerp(starPositions[i][0], targetX, 0.05f);
             starPositions[i][1] = lerp(starPositions[i][1], targetY, 0.05f);
             starPositions[i][2] = lerp(starPositions[i][2], targetZ, 0.05f);
-            
+    
             pushMatrix();
             translate(starPositions[i][0], starPositions[i][1], starPositions[i][2]);
     
@@ -102,6 +121,64 @@ public class Planet extends Visual {
         }
     
         popMatrix();
+        
+        // Draw starry background
+        pushMatrix();
+        translate(width/2, height/2, -500);
+        fill(255);
+        for (int i = 0; i < 1000; i++) {
+            float x = random(-width, width);
+            float y = random(-height, height);
+            float z = random(0, 1000);
+            float size = random(1, 3);
+            ellipse(x, y, size, size);
+        }
+        popMatrix();
+    }
+
+    public PImage createStarsTexture(int w, int h, int numStars) {
+        PImage texture = createImage(w, h, RGB);
+        texture.loadPixels();
+        for (int i = 0; i < numStars; i++) {
+            float x = random(w);
+            float y = random(h);
+            int index = (int) (y * w + x);
+            float size = random(1, 3);
+            int c = color(255, size * 127);
+            texture.pixels[index] = c;
+        }
+        texture.updatePixels();
+        return texture;
+    }
+
+    public PShape createStarsSphere(float radius, int detail, PImage texture) {
+        PShape sphere = createShape(SPHERE, radius);
+        sphere.setTexture(texture);
+        sphereDetail(detail);
+        return sphere;
+    }
+
+    public void drawStarryBackground() {
+        pushMatrix();
+        translate(width / 2, height / 2);
+        shape(starsSphere);
+        popMatrix();
+    }
+
+    public void drawShockwaves() {
+        float bass = getSmoothedBands()[0] * maxPulseSize;
+        smoothedBass = smoothingFactor * bass + (1 - smoothingFactor) * smoothedBass;
+
+        pushMatrix();
+        translate(width / 2, height / 2);
+        noFill();
+        for (int i = 0; i < numShockwaves; i++) {
+            float shockwaveRadius = planetSize + smoothedBass + i * shockwaveThickness;
+            float shockwaveAlpha = map(i, 0, numShockwaves, 200, 0);
+            stroke(255, shockwaveAlpha);
+            ellipse(0, 0, shockwaveRadius * 4, shockwaveRadius * 4);
+        }
+        popMatrix();
     }
     
     public void draw() {
@@ -117,6 +194,24 @@ public class Planet extends Visual {
     
         // Call this if you want to get the average amplitude
         calculateAverageAmplitude();
+        
+        // Set the camera's position
+        float cameraX = cos(angle) * 500;
+        float cameraY = sin(angle) * 500;
+        float cameraZ = 500;
+        camera(cameraX, cameraY, cameraZ, width / 2, height / 2, 0, 0, 1, 0);
+    
+        // Draw starry background
+        drawStarryBackground();
+    
+        // Draw the planet and stars
         drawPlanet();
+
+        // Draw the shockwaves
+        drawShockwaves();
+    
+        // Increment the angle
+        angle += 0.01;
     }
+    
 }
