fd79c9e83fa0254c1416c518e99e34a11854fae6
Ivan <123373115+Gr8ivan@users.noreply.github.com>
2023-05-02 17:51:35 +0100

    Stars Added/Updated

--- java/src/ie/tudublin/Planet.java
+++ java/src/ie/tudublin/Planet.java
@@ -7,6 +7,13 @@ public class Planet extends Visual {
     float smoothedBass = 0;
     float smoothingFactor = 0.3f; // Adjust this value to control the smoothness (0 < smoothingFactor <= 1)
     float maxPulseSize = 0.3f; // Adjust this value to control the maximum increase in size due to pulsing
+    float starDistance = 300;
+    int numStars = 50;
+    float[] starSizes;
+    float[] starSpeeds;
+    float[] starAngles;
+    float[] starDistances;
+    
 
     public void settings() {
         size(1024, 500, P3D);
@@ -26,6 +33,16 @@ public class Planet extends Visual {
 
         // Call this instead to read audio from the microphone
         //startListening();
+        starSizes = new float[numStars];
+        starSpeeds = new float[numStars];
+        starAngles = new float[numStars];
+        starDistances = new float[numStars];
+
+        for (int i = 0; i < numStars; i++) {
+        starSizes[i] = random(5, 15);
+        starSpeeds[i] = random(0.001f, 0.005f);
+        starAngles[i] = random(0, TWO_PI);
+        starDistances[i] = random(starDistance * 1f, starDistance * 3.8f);}
     }
 
     public void keyPressed() {
@@ -46,6 +63,32 @@ public class Planet extends Visual {
         fill(0, 0, 255);
         lights();
         sphere(pulsingSize);
+        popMatrix();
+
+        pushMatrix();
+        translate(width / 2, height / 2);
+        
+        noFill();
+        stroke(255, 100);
+        
+        for (int i = 0; i < numStars; i++) {
+          float angle = starAngles[i] + millis() * starSpeeds[i];
+          float distance = starDistances[i];
+        
+          float x = distance * cos(angle);
+          float y = distance * sin(angle);
+          float z = random(-5, 5);
+        
+          pushMatrix();
+          translate(x, y, z);
+          fill(255);
+          noStroke();
+          sphere(starSizes[i]);
+          popMatrix();
+        
+          ellipse(0, 0, distance * 2, distance * 2);
+        }
+        
         popMatrix();
     }
 
