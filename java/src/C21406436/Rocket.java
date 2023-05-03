b8a85bcf4a6ab0363450b7646e9639346c4dd634
yasseralshimmary79 <alshimmaryyasser@gmail.com>
2023-05-03 22:04:18 +0100

    update Rocket class

--- java/src/C21406436/Rocket.java
+++ java/src/C21406436/Rocket.java
@@ -1,5 +1,4 @@
 package C21406436;
-
 import ie.tudublin.Visual;
 import processing.core.PApplet;
 import processing.core.PVector;
@@ -18,6 +17,13 @@ public class Rocket extends Visual {
     float planetSpeed = 0.2f;
 
     int startTime;
+    PApplet parent;
+
+    public Rocket(PApplet parent) {
+    this.parent = parent;
+}
+    
+
 
     // Add a new Planet class
     class Planet {
@@ -43,7 +49,7 @@ public class Rocket extends Visual {
             }
         }
 
-        void display() {
+        void display(PApplet parent) {
             drawRocketPlanet(x, y, size, color);
         }
     }
@@ -69,8 +75,8 @@ public class Rocket extends Visual {
 
         void display() {
             for (int i = 0; i < trailLength; i++) {
-                fill(0, 0, 255, 255 * (1 - (i / (float) trailLength)));
-                ellipse(position.x, position.y - i * speed, 1, 1);
+                parent.fill(0, 0, 255, 255 * (1 - (i / (float) trailLength)));
+                parent.ellipse(position.x, position.y - i * speed, 1, 1);
             }
         }
     }
@@ -78,34 +84,34 @@ public class Rocket extends Visual {
     ArrayList<Star> stars = new ArrayList<>();
     ArrayList<Planet> planets = new ArrayList<>();
 
+
     public void settings() {
         size(1024, 500, P3D);
+
     }
+
+    
 
     public void setup() {
-        colorMode(HSB); // Set the color mode to HSB
-        noStroke();
-        startMinim();
+        colorMode(HSB, 255);
+        
+        parent.noStroke();
+        
 
         // Call loadAudio to load an audio file to process
-        loadAudio("cantlie-slowed.mp3");
+        
 
         createRandomPlanets(6);
-        startTime = millis();
+        startTime = parent.millis();
     }
 
-    public void keyPressed() {
-        if (key == ' ') {
-            getAudioPlayer().cue(0);
-            getAudioPlayer().play();
-        }
-    }
+    
    
 
-    public void draw() {
-        background(0);
+    public void draw(PApplet parent) {
+        
     
-        drawStars(100);
+        drawStars(100, parent);
     
         for (Star star : stars) {
             star.update();
@@ -113,32 +119,32 @@ public class Rocket extends Visual {
         }
     
         // Check if the elapsed time is greater than 5 seconds (5000 milliseconds)
-        if (millis() - startTime > 5000) {
+        if (parent.millis() - startTime > 5000) {
             for (Planet planet : planets) {
                 planet.update();
-                planet.display();
+                planet.display(parent);
             }
         }
     
         // Calculate the position of the spaceship moving in a circle
-        float spaceshipX = circleRadius * cos(radians(circleAngle));
-        float spaceshipY = circleRadius * sin(radians(circleAngle));
+        float spaceshipX = circleRadius * PApplet.cos(PApplet.radians(circleAngle));
+        float spaceshipY = circleRadius * PApplet.sin(PApplet.radians(circleAngle));
     
-        translate(width / 2, height / 2);
-        translate(spaceshipX, spaceshipY);
-        drawRocket();
+        parent.translate(parent.width / 2, parent.height / 2);
+        parent.translate(spaceshipX, spaceshipY);
+        drawRocket(parent);
     
         circleAngle += 0.4f;
     }    
     
 
-    public void drawStars(int numStars) {
+    public void drawStars(int numStars, PApplet parent) {
         if (stars.size() < numStars) {
             for (int i = stars.size(); i < numStars; i++) {
-                float x = random(width);
-                float y = random(height);
-                float speed = random(1, 3);
-                int trailLength = (int) random(5, 20);
+                float x = parent.random(parent.width);
+                float y = parent.random(parent.height);
+                float speed = parent.random(1, 3);
+                int trailLength = (int) parent.random(5, 20);
                 stars.add(new Star(x, y, speed, trailLength));
             }
         }
@@ -146,11 +152,11 @@ public class Rocket extends Visual {
 
     public void createRandomPlanets(int numPlanets) {
         for (int i = 0; i < numPlanets; i++) {
-            float x = random(width);
-            float y = -random(height) * (i + 1); // Multiply the random height value by (i + 1)
-            float size = random(50, 200);
-            int color = color(random(255), random(255), random(255));
-            float speed = random(1, 3);
+            float x = parent.random(parent.width);
+            float y = -parent.random(parent.height) * (i + 1); // Multiply the random height value by (i + 1)
+            float size = parent.random(50, 200);
+            int color = parent.color(parent.random(255), parent.random(255), parent.random(255));
+            float speed = parent.random(1, 3);
             planets.add(new Planet(x, y, size, color, speed));
         }
     }
@@ -158,77 +164,77 @@ public class Rocket extends Visual {
     
 
     public void drawRocketPlanet(float x, float y, float size, int planetColor) {
-        pushMatrix();
-        translate(x, y);
-        noStroke();
+        parent.pushMatrix();
+        parent.translate(x, y);
+        parent.noStroke();
     
         for (int i = 0; i < 360; i += 5) {
-            float angle = radians(i);
-            float angleNext = radians(i + 5);
+            float angle = PApplet.radians(i);
+            float angleNext = PApplet.radians(i + 5);
     
-            float x1 = cos(angle) * size / 2;
-            float y1 = sin(angle) * size / 2;
-            float x2 = cos(angleNext) * size / 2;
-            float y2 = sin(angleNext) * size / 2;
+            float x1 = PApplet.cos(angle) * size / 2;
+            float y1 = PApplet.sin(angle) * size / 2;
+            float x2 = PApplet.cos(angleNext) * size / 2;
+            float y2 = PApplet.sin(angleNext) * size / 2;
     
-            float distance1 = dist(x1, y1, 0, 0);
-            float distance2 = dist(x2, y2, 0, 0);
+            float distance1 = PApplet.dist(x1, y1, 0, 0);
+            float distance2 = PApplet.dist(x2, y2, 0, 0);
     
-            int shade1 = color(hue(planetColor), map(distance1, 0, size / 2, 0, 100), brightness(planetColor) * (1 - (i / 360.0f)));
-            int shade2 = color(hue(planetColor), map(distance2, 0, size / 2, 0, 100), brightness(planetColor) * (1 - ((i + 5) / 360.0f)));
+            int shade1 = parent.color(parent.hue(planetColor), PApplet.map(distance1, 0, size / 2, 0, 100), parent.brightness(planetColor) * (1 - (i / 360.0f)));
+            int shade2 = parent.color(parent.hue(planetColor), PApplet.map(distance2, 0, size / 2, 0, 100), parent.brightness(planetColor) * (1 - ((i + 5) / 360.0f)));
     
-            beginShape();
-            fill(shade1);
-            vertex(x1, y1);
-            fill(shade2);
-            vertex(x2, y2);
-            fill(planetColor);
-            vertex(0, 0);
-            endShape(CLOSE);
+            parent.beginShape();
+            parent.fill(shade1);
+            parent.vertex(x1, y1);
+            parent.fill(shade2);
+            parent.vertex(x2, y2);
+            parent.fill(planetColor);
+            parent.vertex(0, 0);
+            parent.endShape(CLOSE);
         }
     
         // Glowing aura around the planet
         for (int i = 0; i < 10; i++) {
-            int glowColor = color(hue(planetColor), 100, 100, 50 - i * 5);
-            fill(glowColor);
-            ellipse(0, 0, size + i * 10, size + i * 10);
+            int glowColor = parent.color(parent.hue(planetColor), 100, 100, 50 - i * 5);
+            parent.fill(glowColor);
+            parent.ellipse(0, 0, size + i * 10, size + i * 10);
         }
     
-        popMatrix();
+        parent.popMatrix();
     }
     
       
 
-      public void drawRocket() {
-        fill(0, 0, 255); // Set the rocket color using HSB values
+      public void drawRocket(PApplet parent) {
+        parent.fill(0, 0, 255); // Set the rocket color using HSB values
         
         // Body of the rocket
-        beginShape();
-        vertex(0, -20);
-        vertex(-10, 10);
-        vertex(10, 10);
-        endShape(CLOSE);
+        parent.beginShape();
+        parent.vertex(0, -20);
+        parent.vertex(-10, 10);
+        parent.vertex(10, 10);
+        parent.endShape(CLOSE);
     
         // Left wing
-        beginShape();
-        vertex(-10, 10);
-        vertex(-30, 20);
-        vertex(-20, 10);
-        endShape(CLOSE);
+        parent.beginShape();
+        parent.vertex(-10, 10);
+        parent.vertex(-30, 20);
+        parent.vertex(-20, 10);
+        parent.endShape(CLOSE);
     
         // Right wing
-        beginShape();
-        vertex(10, 10);
-        vertex(30, 20);
-        vertex(20, 10);
-        endShape(CLOSE);
+        parent.beginShape();
+        parent.vertex(10, 10);
+        parent.vertex(30, 20);
+        parent.vertex(20, 10);
+        parent.endShape(CLOSE);
     
         // Engine trail
         for (int i = 0; i < 10; i++) {
-            float engineTrailSize = 10 + 5 * sin(radians(millis() / 5)) * (1 - (i / 10.0f));
+            float engineTrailSize = 10 + 5 * PApplet.sin(PApplet.radians(millis() / 5)) * (1 - (i / 10.0f));
             float engineTrailOffset = 10 + 5 * i;
-            fill(255, 225, 180, 255 - i * 20); // Added transparency to the trail
-            ellipse(0, 10 + engineTrailOffset, engineTrailSize, engineTrailSize);
+            parent.fill(255, 225, 180, 255 - i * 20); // Added transparency to the trail
+            parent.ellipse(0, 10 + engineTrailOffset, engineTrailSize, engineTrailSize);
         }
     }
     
